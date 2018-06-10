77d2f50e0903503e8ef584da6dd80a581db6b5f6
@Vika@ <frantsuhvika@gmail.com>
2018-06-10 17:16:57 +0300

    add options menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame()
+	{
+		
+	}
+
+	public void Options()
+	{
+		
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+		Debug.Log("QUIT");
+	}
+}
