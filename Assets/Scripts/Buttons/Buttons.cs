de2652bcda0a475a4d1abddebea38754f47da061
@Vika@ <frantsuhvika@gmail.com>
2018-06-12 13:14:10 +0300

    add button BG sound off

--- Assets/Scripts/Buttons/Buttons.cs
+++ Assets/Scripts/Buttons/Buttons.cs
@@ -16,4 +16,10 @@ public class Buttons : MonoBehaviour
     {
         GetComponent<SpriteRenderer>().sprite = layer_blue;
     }
+
+    public void onPause()
+    {
+        
+        Debug.Log("Pause");
+    }
 }
